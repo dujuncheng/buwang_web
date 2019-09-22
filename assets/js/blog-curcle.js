040e01337700bce7ca4dd7e36f12b4dd25a023ba
juncheng.du <juncheng.du@beibei.com>
2019-09-22 14:01:09 +0800

    add

---
+++ assets/js/blog-curcle.js
@@ -0,0 +1,11 @@
+let vm = new Vue({
+	el: '#app',
+	data() {
+		return {
+			dudu: 'dddd'
+		}
+	}
+})
+
+console.log(vm)
+// vm.$mount();
---
+++ assets/js/blog-single.js
@@ -0,0 +1,15 @@
+const require
+
+let vm = new Vue({
+	el: '#app',
+	data() {
+		return {
+			dudu: 'dddd'
+		}
+	},
+	created() {
+	
+	}
+})
+
+console.log(vm)
