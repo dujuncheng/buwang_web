51e8855bd3cc8077bd39c789015e2f5e94fd77f1
juncheng.du <juncheng.du@beibei.com>
2019-09-27 20:55:58 +0800

    add

--- assets/js/blog-single.js
+++ assets/js/blog-single.js
@@ -30,6 +30,17 @@ let vm = new Vue({
 			},
 		}
 	},
+	filters: {
+		formatData(timeStamp) {
+			let date = new Date(timeStamp);
+			let year = date.getFullYear();
+			let month = date.getMonth() + 1;
+			let day = date.getDate();
+			let hour = date.getHours();
+			let minutes = date.getMinutes();
+			return `${year}-${month}-${day} ${hour}:${minutes}`
+		}
+	},
 	created() {
 		this.getNoteId();
 		this.requestBlog();
